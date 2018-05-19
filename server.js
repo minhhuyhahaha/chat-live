16eb051ad0b2c4f2c67cc0d4de4d5a5b0db71303
minhhuyhahaha <39334276+minhhuyhahaha@users.noreply.github.com>
2018-05-19 15:28:25 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 var express = require("express");
 var app = new express();
-var port = 8080;
+var port = process.env.PORT || 8080;
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
 
 app.use('/',express.static(__dirname+ '/sources'));
 
 app.get("/", function (req, res) {
-	res.sendFile(__dirname+'/sources/index.html');
+    res.sendFile(__dirname+'/sources/index.html');
 })
 
 io.on('connection', function (socket) {
-	socket.on('stream', function (chunk) {
-		socket.broadcast.emit('stream',chunk);
-	})
+    socket.on('stream', function (chunk) {
+        socket.broadcast.emit('stream',chunk);
+    })
 })
 
 http.listen(port, function () {
-	console.log('Đang lắng nghe kết nối: ', port);
+    console.log('Đang lắng nghe kết nối: ', port);
 })
